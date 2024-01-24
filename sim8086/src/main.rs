2baef2c14a2e6e2861ac481c589901595e3b142d
Javier Figueroa <javier.o.figueroa@gmail.com>
2024-01-23 22:32:49 -0400

    Refactored solution

--- sim8086/src/main.rs
+++ sim8086/src/main.rs
@@ -1,13 +1,6 @@
 use std::fs;
 use std::env;
 
-const REGISTER: [&'static str; 16] = [
-    "al", "cl", "dl", "bl", 
-    "ah", "ch", "dh", "bh", 
-    "ax", "cx", "dx", "bx", 
-    "sp", "bp", "si", "di" 
-];
-
 fn main() {
     let path = env::args().nth(1).unwrap();
     let bytes = fs::read(path).unwrap();
@@ -15,23 +8,30 @@ fn main() {
     while i < bytes.len() {
         let byte_1 = bytes[i];
         let byte_2 = bytes[i+1];
+        let instruction = mov_reg_mem_to_from_reg(byte_1, byte_2);
+        println!("{}", instruction);
 
-        let _op_code = byte_1 >> 2;
-        let _direction = (byte_1 & 0b00000010) >> 1;
-        let wide = byte_1 & 0b00000001;
-
-        let _mode = byte_2 >> 6;
-        let reg = (byte_2 & 0b00111000) >> 3;
-        let reg_mem = byte_2 & 0b00000111;
-        let reg_str = register_decoder(reg, wide);
-        let reg_mem_str = register_decoder(reg_mem, wide);
-
-        println!("mov {}, {}", reg_mem_str, reg_str);
         i += 2;
     }
 }
 
+fn mov_reg_mem_to_from_reg(byte_1: u8, byte_2: u8) -> String {
+    let wide = byte_1 & 0b00000001;
+    let reg = (byte_2 & 0b00111000) >> 3;
+    let reg_mem = byte_2 & 0b00000111;
+    let reg_str = register_decoder(reg, wide);
+    let reg_mem_str = register_decoder(reg_mem, wide);
+
+    return format!("mov {}, {}", reg_mem_str, reg_str);
+}
+
 fn register_decoder(reg: u8, wide: u8) -> &'static str {
+    const REGISTER: [&'static str; 16] = [
+        "al", "cl", "dl", "bl", 
+        "ah", "ch", "dh", "bh", 
+        "ax", "cx", "dx", "bx", 
+        "sp", "bp", "si", "di" 
+    ];
     let reg_index = ((wide << 3) | reg) as usize;
     return REGISTER[reg_index];
 }
