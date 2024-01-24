4f993ffd67daeab2716b0fb7733338c4b3d0ab7d
Javier Figueroa <javier.o.figueroa@gmail.com>
2024-01-24 00:37:10 -0400

    Added bitmatch dependency and refactored

--- sim8086/src/main.rs
+++ sim8086/src/main.rs
@@ -1,28 +1,36 @@
 use std::fs;
 use std::env;
+use bitmatch::bitmatch;
 
 fn main() {
     let path = env::args().nth(1).unwrap();
     let bytes = fs::read(path).unwrap();
     let mut i = 0;
     while i < bytes.len() {
-        let byte_1 = bytes[i];
-        let byte_2 = bytes[i+1];
-        let instruction = mov_reg_mem_to_from_reg(byte_1, byte_2);
-        println!("{}", instruction);
-
-        i += 2;
+        i = instruction_decoder(&bytes, i);
     }
 }
 
+#[bitmatch]
 fn mov_reg_mem_to_from_reg(byte_1: u8, byte_2: u8) -> String {
-    let wide = byte_1 & 0b00000001;
-    let reg = (byte_2 & 0b00111000) >> 3;
-    let reg_mem = byte_2 & 0b00000111;
-    let reg_str = register_decoder(reg, wide);
-    let reg_mem_str = register_decoder(reg_mem, wide);
+    #[bitmatch]
+    let "???????w" = byte_1;
+    #[bitmatch]
+    let "??rrrmmm" = byte_2;
+
+    let reg_str = register_decoder(r, w);
+    let reg_mem_str = register_decoder(m, w);
 
     return format!("mov {}, {}", reg_mem_str, reg_str);
+}
+
+fn instruction_decoder(bytes: &Vec<u8>, current_byte_index: usize) -> usize {
+        let byte_1 = bytes[current_byte_index];
+        let byte_2 = bytes[current_byte_index+1];
+        let instruction = mov_reg_mem_to_from_reg(byte_1, byte_2);
+        println!("{}", instruction);
+
+        return current_byte_index + 2;
 }
 
 fn register_decoder(reg: u8, wide: u8) -> &'static str {
